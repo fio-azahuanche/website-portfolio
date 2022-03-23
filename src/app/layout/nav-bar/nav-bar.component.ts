2fdaafa3cfd4adf94d401ba4709907d42aeb8d86
unknown <fazahuanchef@gmail.com>
2022-03-23 02:11:42 -0500

    add all responsive

--- src/app/layout/nav-bar/nav-bar.component.ts
+++ src/app/layout/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   logo: string ='<FA/>';
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+  navigateToHome(){
+    this.router.navigate(['']);
+  }
+    navigateToAbout(){
+      this.router.navigate(['/about']);
+    }
+    navigateToResume(){
+      this.router.navigate(['/resume']);
+    }
+    navigateToSkills(){
+      this.router.navigate(['/skills']);
+    }
+    navigateToPorfolio(){
+      this.router.navigate(['/projects']);
+    }
+    navigateToContact(){
+      this.router.navigate(['/contact']);
+    }
+
+  isOpen = false;
+
+  toggle():void {
+    this.isOpen = !this.isOpen;
   }
 
 }
